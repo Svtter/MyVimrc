438d84b5b67f1f04429eef3ba295be2d44b46c1e
svtter <svtter@qq.com>
2014-08-09 11:04:16 +0800

    addVIM

---
+++ .vim/template/cpp/cppconfig.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+#define INF 0xffffff
+#define lln long long
+
+#ifdef ONLINE_JUDGE
+#define FOI(file) 0
+#define FOW(file) 0
+#else
+#define FOI(file) freopen(file,"r",stdin);
+#define FOW(file) freopen(file,"w",stdout);
+#endif
+
+using namespace std;
+
+
+int main()
+{
+    //FOI("input");
+    //FOW("output");
+    //write your programme here
+
+    return 0;
+}
