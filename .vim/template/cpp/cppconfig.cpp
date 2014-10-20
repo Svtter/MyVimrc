3dd09ee1c5d8f20787f257f61f5ef1b12736a5bb
svtter <svtter@qq.com>
2014-10-20 11:46:48 +0800

    the newest

--- .vim/template/cpp/cppconfig.cpp
+++ .vim/template/cpp/cppconfig.cpp
@@ -9,19 +9,28 @@
 #include <bitset>
 using namespace std;
 
+// 大数，内存处理
 #define INF 0x3f3f3f3f
 #define ll long long int
 #define MEM(a) memset(a, 0, sizeof(a))
 #define MEMM(a) memset(b, -1, sizeof(b))
-#define DBG(x, n) cout << (x) << " " << (n) << endl;
+#define DEB(x, n) cout << (x) << " " << (n) << endl;
 
+// 字符串处理
 #define SL(a) strlen(a)
+#define SCM(a, b) strcmp(a, b)
+#define SCP(a, b) strcpy(a, b)
+
+// 输入函数
 #define RS(s) scanf("%s", (s))
-#define PI(r) printf("%d\n", (r))
 #define RI(a) scanf("%d", &(a))
+#define PI(r) printf("%d\n", (r))
+#define PS(s) printf("%s\n", (s))
 #define RII(a, b) scanf("%d%d", &(a), &(b))
 #define RIII(a, b, c) scanf("%d%d%d", &(a), &(b), &(c))
 
+// 应对ONLINE_JUDGE, 读入读出处理
+// 利用bash脚本调试数据
 #ifdef ONLINE_JUDGE
 #define FOI(file) 0
 #define FOW(file) 0
@@ -30,11 +39,16 @@ using namespace std;
 #define FOW(file) freopen(file,"w",stdout);
 #endif
 
+// 定义常用工作变量
+int t, i, j, k;
+
 int main()
 {
     //FOI("input");
     //FOW("output");
     //write your programme here
+
+
 
     return 0;
 }
